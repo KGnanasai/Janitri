fb4b9a3f3cf60b59c1d17b47ef4a1329984aaddc
KGnanasai <112232239+KGnanasai@users.noreply.github.com>
2025-09-04 11:34:31 +0530

    Create BaseTest.java

---
+++ Automation Framework Setup/BaseTest.java
@@ -0,0 +1,25 @@
+package com.janitri.tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://dev-dash.janitri.in");
+        driver.manage().window().maximize();
+    }
+
+    @AfterMethod
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
